819e9be7d447b966a3f566af7c8da4c63c6d5757
Dave Letorey <daveletorey@gmail.com>
2018-01-04 15:33:30 +0000

    added an if statement to script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -3,9 +3,18 @@ var a = 1;
 var b = 2;
 var result =  a + b;
 alert(result); */
-
+/*
 var myName = "Dave"; // string data type
 var number = 1; // number data type
 var boolean = false; // boolean data type
 var boolean1 = true; // boolean data type
 var a; // undefined variable
+*/
+
+var a = 10;
+var b = 20;
+
+if(a < b){
+    var result = a + b;
+    document.write("The result of a and b is: ", result);
+}
