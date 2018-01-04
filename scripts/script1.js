98c88b575418c4ec82a8aa39109cd1b4d982a3be
Dave Letorey <daveletorey@gmail.com>
2018-01-04 15:28:07 +0000

    added different data type variables to script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,5 +1,11 @@
-
+/*
 var a = 1;
 var b = 2;
 var result =  a + b;
-alert(result);
+alert(result); */
+
+var myName = "Dave"; // string data type
+var number = 1; // number data type
+var boolean = false; // boolean data type
+var boolean1 = true; // boolean data type
+var a; // undefined variable
