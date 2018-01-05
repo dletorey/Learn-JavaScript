b7105c8a936142002b325eeca99e5f3c68ce594f
Dave Letorey <daveletorey@gmail.com>
2018-01-05 15:33:59 +0000

    added a longhand array to array.js

---
+++ scripts/array.js
@@ -0,0 +1,10 @@
+/* Longhand array */
+var studentName = [];
+studentName[0] = "Dave";
+studentName[1] = "Dom";
+studentName[2] = "Hank";
+studentName[3] = "Hamish";
+studentName[4] = "Oli";
+
+document.write(studentName[3]);
+
