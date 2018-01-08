dde17f133daa494929d869188a7ac76de01fbf73
Dave Letorey <daveletorey@gmail.com>
2018-01-08 15:49:01 +0000

    added random number generator in numbers.js

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -1,7 +1,5 @@
 var num1 = 5;
 var num2 = 3;
-var num3 = "5";
-var num4 = "6";
-document.write(num1 + num2 + "<br>");
-document.write(num3 + num4 + "<br>");
-document.write(num1 * num3);
+
+var value = Math.random();
+document.write(value);
