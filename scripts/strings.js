e17c3a354e75878c705545518c25de5092c9829a
Dave Letorey <daveletorey@gmail.com>
2018-01-08 16:17:06 +0000

    added get new Date & getFullYear to strings.js

--- scripts/strings.js
+++ scripts/strings.js
@@ -2,4 +2,9 @@ var a = "This is a string that has a \"Quote in it\"";
 var b = a.length;
 console.log(b);
 console.log(a.charAt(10));
-console.log(a.search("Q"));
+console.log(a.search("\""));
+
+var date = new Date();
+console.log(date);
+var getYear = date.getFullYear();
+console.log(getYear);
