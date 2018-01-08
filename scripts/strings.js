cf8a98bb7d0acae057ad38fab598fc7cb874a56d
Dave Letorey <daveletorey@gmail.com>
2018-01-08 16:27:19 +0000

    added get new Boolean to strings.js

--- scripts/strings.js
+++ scripts/strings.js
@@ -1,10 +1,18 @@
+/* Strings
 var a = "This is a string that has a \"Quote in it\"";
 var b = a.length;
 console.log(b);
 console.log(a.charAt(10));
 console.log(a.search("\""));
+*/
 
+/* Dates
 var date = new Date();
 console.log(date);
 var getYear = date.getFullYear();
 console.log(getYear);
+ */
+
+ /* Booleans */
+ var bool = new Boolean();
+ console.log(bool.valueOf);
