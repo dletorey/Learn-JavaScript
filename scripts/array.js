7f06b3650a569f0fc0f79a94b2029abeb1a62141
Dave Letorey <dletorey@squiz.co.uk>
2018-01-23 14:32:53 +0000

    final commit for this course

--- scripts/array.js
+++ scripts/array.js
@@ -15,3 +15,13 @@ var newArray = Array(5);
 
 // teacherName.length();
 document.write(teacherName.length);
+
+function maxOfThree(num1, num2, num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
